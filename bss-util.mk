31f38b9fa4fb51f79dddce0ea486be4effe779b7
Erik McClure <erikm@fundament.software>
2013-09-07 08:12:14 +0000

    Fixing shiny on linux

--- bss-util.mk
+++ bss-util.mk
@@ -2,15 +2,15 @@ TARGET := libbss_util.so
 SRCDIR := bss_util
 BUILDDIR := bin
 OBJDIR := bin/obj
-#C_SRCS := $(wildcard *.c)
-C_SRCS := bss_util_c.c INIParser.c
-#CXX_SRCS := $(wildcard *.cpp)
-CXX_SRCS := bss_DebugInfo.cpp bss_Log.cpp bss_util.cpp cCmdLineArgs.cpp cHighPrecisionTimer.cpp cINIentry.cpp cINIsection.cpp cINIstorage.cpp
-INCLUDE_DIRS := include
+C_SRCS := $(wildcard $(SRCDIR)/*.c)
+#C_SRCS := $(SRCDIR)/bss_util_c.c $(SRCDIR)/INIParser.c
+CXX_SRCS := $(wildcard $(SRCDIR)/*.cpp)
+#CXX_SRCS := bss_DebugInfo.cpp bss_Log.cpp bss_util.cpp cCmdLineArgs.cpp cHighPrecisionTimer.cpp cINIentry.cpp cINIsection.cpp cINIstorage.cpp
+INCLUDE_DIRS := include include/shiny
 LIBRARY_DIRS := 
 LIBRARIES := rt
 
-CPPFLAGS += -fPIC -Wall -Wno-attributes -Wno-unknown-pragmas -Wno-reorder -Wno-missing-braces -std=gnu++0x
+CPPFLAGS += -fPIC -D BSS_UTIL_EXPORTS -Wall -Wno-attributes -Wno-unknown-pragmas -Wno-reorder -Wno-missing-braces -std=gnu++0x
 LDFLAGS += -shared
 
 include base.mk

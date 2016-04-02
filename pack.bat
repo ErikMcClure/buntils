6961642dc48075620fc4af85a62c2507431d8f57
Erik McClure <erikm@fundament.software>
2016-04-02 02:35:10 -0700

    Minor project fixups

--- pack.bat
+++ pack.bat
@@ -12,21 +12,18 @@ XCOPY "bss-util\*.rc" "..\Packages\bss-util\bss-util" /C /I /R /Y
 md "..\Packages\bss-util\include"
 md "..\Packages\bss-util\doc"
 md "..\Packages\bss-util\bin"
+md "..\Packages\bss-util\bin32"
 md "..\Packages\bss-util\test"
 
 XCOPY "include\*.h" "..\Packages\bss-util\include" /S /C /I /R /Y
-DEL "..\Packages\bss-util\include\bss_sort.h"
-DEL "..\Packages\bss-util\include\cIntervalTree.h"
-DEL "..\Packages\bss-util\include\cHuffmanTree.h"
 XCOPY "doc\*.txt" "..\Packages\bss-util\doc" /S /C /I /R /Y
-XCOPY "bin\*.dll" "..\Packages\bss-util\bin" /C /I /Y
-XCOPY "bin\*.lib" "..\Packages\bss-util\bin" /C /I /Y
-XCOPY "bin\*.pdb" "..\Packages\bss-util\bin" /C /I /Y
-XCOPY "bin\blank.txt" "..\Packages\bss-util\bin\" /C /I /Y
-DEL "..\Packages\bss-util\bin\test_d.*"
-DEL "..\Packages\bss-util\bin\test.*"
-DEL "..\Packages\bss-util\bin\test64.*"
-DEL "..\Packages\bss-util\bin\test64_d.*"
-XCOPY "bin\*.exe" "..\Packages\bss-util\bin\" /C /I /Y
+XCOPY "bin\bss-util*.dll" "..\Packages\bss-util\bin" /C /I /Y
+XCOPY "bin\bss-util*.lib" "..\Packages\bss-util\bin" /C /I /Y
+XCOPY "bin\bss-util*.pdb" "..\Packages\bss-util\bin" /C /I /Y
+XCOPY "bin32\bss-util*.dll" "..\Packages\bss-util\bin32" /C /I /Y
+XCOPY "bin32\bss-util*.lib" "..\Packages\bss-util\bin32" /C /I /Y
+XCOPY "bin32\bss-util*.pdb" "..\Packages\bss-util\bin32" /C /I /Y
+XCOPY "bin\test*.exe" "..\Packages\bss-util\bin" /C /I /Y
+XCOPY "bin32\test*.exe" "..\Packages\bss-util\bin32" /C /I /Y
 
 Pause

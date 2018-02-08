dd647c7f7689b44f9f0f1a9d0b4604b533e6c504
Erik McClure <erikm@blackspherestudios.com>
2018-02-07 18:08:53 -0800

    Update copyright

--- include/bss-util/PriorityQueue.h
+++ include/bss-util/PriorityQueue.h
@@ -1,4 +1,4 @@
-// Copyright ©2017 Black Sphere Studios
+// Copyright ©2018 Black Sphere Studios
 // For conditions of distribution and use, see copyright notice in "bss_util.h"
 
 #ifndef __PRIORITY_QUEUE_H__BSS__
